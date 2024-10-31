106f54774542f98acf61e83cec9a7260c0e62600
pervitin <mithrandir1861@vp.pl>
2024-10-31 10:20:02 +0100

    add simple dense model

--- src/spaceflights/pipelines/dense_model/nodes.py
+++ src/spaceflights/pipelines/dense_model/nodes.py
@@ -2,3 +2,34 @@
 This is a boilerplate pipeline 'dense_model'
 generated using Kedro 0.19.9
 """
+
+import logging
+import typing as t
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import tensorflow as tf
+from tensorflow.keras import models, layers # type: ignore
+
+def split_data(df: pd.DataFrame, parameters: t.Dict) -> t.Tuple:
+    X = df[parameters["features"]]
+    y = df["price"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=parameters["test_size"]
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def train_model(X_train: pd.DataFrame, y_train: pd.Series):
+    model = models.Sequential([layers.Dense(10), layers.Dense(1)])
+    model.compile(loss="mae", optimizer="adam", metrics=["mae"])
+    model.fit(x=X_train, y=y_train, epochs=100)
+    return model
+
+
+def evaluate_model(model, X_test: pd.DataFrame, y_test: pd.Series):
+    y_pred = model.predict(X_test)
+    score = r2_score(y_true=y_test, y_pred=y_pred)
+    logger = logging.getLogger(__name__)
+    logger.info("Model has a coefficient R^2 of %.3f on test data.", score)
--- src/spaceflights/pipelines/dense_model/pipeline.py
+++ src/spaceflights/pipelines/dense_model/pipeline.py
@@ -3,8 +3,26 @@ This is a boilerplate pipeline 'dense_model'
 generated using Kedro 0.19.9
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+
+from .nodes import split_data, train_model, evaluate_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=split_data,
+                inputs=["model_input_table", "params:model_options"],
+                outputs=["X_train", "X_test", "y_train", "y_test"],
+            ),
+            node(
+                func=train_model, inputs=["X_train", "y_train"], outputs="dense_model"
+            ),
+            node(
+                func=evaluate_model,
+                inputs=["dense_model", "X_test", "y_test"],
+                outputs=None,
+            ),
+        ]
+    )
